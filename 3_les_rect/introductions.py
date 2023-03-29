549f1a9f742b6d1dc61394d2a81bbf1765f5a090
Entropie67 <olivier.elophe@gmail.com>
2023-03-29 11:47:37 +0200

    click rectangle

--- 3_les_rect/introductions.py
+++ 3_les_rect/introductions.py
@@ -1,4 +1,19 @@
-
 # Les objets de types Rect sont fondamentaux !!!!!
+################################################################################################
+# Pour créer une zone rectangulaire on indique le coin en haut à gauche left, top
+# et la largeur et la hauteur
+# zone_rect1 = pygame.Rect(left, top, width, height)
+
+# On peut aussi créer un Rect à partir d'une surface :
+
+# surface1 = ...
+# surface1_rect = Rect(surface1)
+# ou
+# surface1_rect = surface1.get_rect()
+
+# DOCUMENTATION :   https://www.pygame.org/docs/ref/rect.html
+################################################################################################
+
+import pygame
 
 
